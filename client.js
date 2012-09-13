7402b62840e2372ed3dda281a457c6e62c50a72a
stevoland <stevoland@gmail.com>
2012-09-13 17:24:22 +0100

    Basic click events working

--- client.js
+++ client.js
@@ -1,93 +1,278 @@
-(function (d, w) {
+(function (w, d) {
 
-    var socket;
+	var socket,
+		scripts = document.getElementsByTagName('script'),
+		script  = scripts[scripts.length-1],
+		src     = script.src,
+		key,
+		isLeader;
 
-    // http://blog.stevenlevithan.com/archives/parseuri
-    function parseUri (str) {
-        var o   = parseUri.options,
-            m   = o.parser[o.strictMode ? "strict" : "loose"].exec(str),
-            uri = {},
-            i   = 14;
+	// http://blog.stevenlevithan.com/archives/parseuri
+	var parseURI = (function () {
+		var options = {
+			strictMode: false,
+			key: ["source","protocol","authority","userInfo","user","password","host","port","relative","path","directory","file","query","anchor"],
+			q:   {
+				name:   "queryKey",
+				parser: /(?:^|&)([^&=]*)=?([^&]*)/g
+			},
+			parser: {
+				strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
+				loose:  /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
+			}
+		};
 
-        while (i--) uri[o.key[i]] = m[i] || "";
+		function parseURI (str) {
+			var o   = options,
+				m   = o.parser[o.strictMode ? "strict" : "loose"].exec(str),
+				uri = {},
+				i   = 14;
 
-        uri[o.q.name] = {};
-        uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {
-            if ($1) uri[o.q.name][$1] = $2;
-        });
+			while (i--) uri[o.key[i]] = m[i] || "";
 
-        return uri;
-    }
+			uri[o.q.name] = {};
+			uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {
+				if ($1) uri[o.q.name][$1] = $2;
+			});
 
-    parseUri.options = {
-        strictMode: false,
-        key: ["source","protocol","authority","userInfo","user","password","host","port","relative","path","directory","file","query","anchor"],
-        q:   {
-            name:   "queryKey",
-            parser: /(?:^|&)([^&=]*)=?([^&]*)/g
-        },
-        parser: {
-            strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
-            loose:  /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
-        }
-    };
+			return uri;
+		}
 
-    var scripts = document.getElementsByTagName('script');
-    var script = scripts[scripts.length-1];
-    var src = script.src;
+		return parseURI;
+	}());
 
-    console.info(src);
+	
 
 
-    var id = parseUri(window.location).queryKey.gsID || parseUri(script.src).queryKey.gsID;
+	// https://github.com/ded/domready
 
-    if (id) {
-        console.info(id);
+	// http://stackoverflow.com/a/6158050
+	function getCSSSelector(el) {
+		var names = [];
+		while (el.parentNode) {
+			if (el.id) {
+				names.unshift('#' + el.id);
+				break;
+			} else {
+				if (el == el.ownerDocument.documentElement || el == el.ownerDocument.body) {
+					names.unshift(el.tagName);
+				} else {
+					for (var c=1, e=el; e.previousElementSibling; e=e.previousElementSibling, c++);
+					names.unshift(el.tagName + ':nth-child(' + c + ')');
+				}
+				el = el.parentNode;
+			}
+		}
+		return names.join(' > ');
+	}
 
-        socket = io.connect('http://localhost:8001');
+	function receiveEvent (e) {
+		var i;
 
-        socket.on('ready', function () {
-            //socket.emit('msg', 'new message');
+		if (e.gsElements) {
+			i = e.gsElements.length;
+			while (i--) {
+				console.info(e[e.gsElements[i]]);
+				e[e.gsElements[i]] = d.querySelector(e[e.gsElements[i]]);
+				console.info(e[e.gsElements[i]]);
+			}
+			delete e.gsElements;
+		}
+		if (e.gsDocuments) {
+			i = e.gsDocuments.length;
+			while (i--) {
+				e[e.gsDocuments[i]] = d;
+			}
+			delete e.gsDocuments;
+		}
+		if (e.gsWindows) {
+			i = e.gsWindows.length;
+			while (i--) {
+				e[e.gsWindows[i]] = w;
+			}
+			delete e.gsWindows;
+		}
 
-            d.addEventListener('click', function (e) {
-                console.info(e);
+		e.gsSimulated = true;
 
-                var node = e.target,
-                    anchor;
+		simulateEvent(e.originalTarget || e.target, e.type, e);
+	}
 
-                while (node) {
-                    if (node.nodeName.toUpperCase() === 'A') {
-                        anchor = node;
-                        break;
-                    } else {
-                        node = node.parentNode;
-                    }
-                }
+	function sendEvent(ev) {
+		var e = ev || w.event,
+			e2 = {
+				gsElements: [],
+				gsDocuments: [],
+				gsWindows: [],
+				gsSimulated: true
+			},
+			i;
 
-                if (anchor && anchor.href) {
-                    socket.emit('href', anchor.href);
-                }
+		if (e.gsSimulated) {
+			return true;
+		}
 
-                e.preventDefault();
-            }, false);
+		for (i in e) {
+			if (i === 'originalTarget' || i === 'rangeParent' ) {
+				// Firefox throws errors from text inputs due to XUL elements
+				// ignore these
+			} else if (e[i] instanceof HTMLElement) {
+				e2[i] = getCSSSelector(e[i]);
+				e2.gsElements.push(i);
+			} else if (e[i] === document) {
+				e2[i] = 'document';
+				e2.gsDocuments.push(i);
+			} else if (e[i] === window) {
+				e2[i] = 'window';
+				e2.gsWindows.push(i);
+			} else if (e[i] === null) {
+				e2[i] = e[i];
+			} else if (typeof e[i] === 'function') {
 
-            window.onpopstate = function(event) {
-                console.info('here');
-              socket.emit('href', d.location);
-              return false;
-            };
-            /*window.onbeforeunload = function (e) {
-                console.info(e);
-                return false;
-            }*/
-        });
+			} else if (typeof e[i] === 'object') {
+				e2[i] = e[i].toString();
+			}  else {
+				e2[i] = e[i];
+			}
+		}
+		
+		socket.emit('event', e2);
+	}
 
-        socket.emit('join', id);
+	var simulateEvent = (function (w, d) {
+		var eventMatchers = {
+				'HTMLEvents': /^(?:load|unload|abort|error|select|change|submit|reset|focus|blur|resize|scroll)$/,
+				'MouseEvents': /^(?:click|dblclick|mouse(?:down|up|over|move|out))$/
+			},
+			defaultOptions = {
+				pointerX: 0,
+				pointerY: 0,
+				button: 0,
+				ctrlKey: false,
+				altKey: false,
+				shiftKey: false,
+				metaKey: false,
+				bubbles: true,
+				cancelable: true
+			};
 
-        socket.on('href', function (href) {
-            window.location = href;
-        });
-    }
+		function extend(destination, source) {
+			for (var property in source) {
+				destination[property] = source[property];
+			}
+
+			return destination;
+		}
+
+		function simulate(element, eventName)
+		{
+			var options = extend(defaultOptions, arguments[2] || {});
+			var oEvent, eventType = null;
+
+			for (var name in eventMatchers)
+			{
+				if (eventMatchers[name].test(eventName)) { eventType = name; break; }
+			}
+
+			if (!eventType)
+				throw new SyntaxError('Only HTMLEvents and MouseEvents interfaces are supported');
+
+			if (document.createEvent)
+			{
+				oEvent = document.createEvent(eventType);
+				if (eventType == 'HTMLEvents')
+				{
+					oEvent.initEvent(eventName, options.bubbles, options.cancelable);
+				}
+				else
+				{
+					oEvent.initMouseEvent(eventName, options.bubbles, options.cancelable, d.defaultView,
+						options.button, options.pointerX, options.pointerY, options.pointerX, options.pointerY,
+						options.ctrlKey, options.altKey, options.shiftKey, options.metaKey, options.button, element);
+				}
+				oEvent = extend(oEvent, options);
+				element.dispatchEvent(oEvent);
+			}
+			else
+			{
+				options.clientX = options.pointerX;
+				options.clientY = options.pointerY;
+				var evt = d.createEventObject();
+				oEvent = extend(evt, options);
+				element.fireEvent('on' + eventName, oEvent);
+			}
+			return element;
+		}
+
+		return simulate;
+	}(w, d));
+	
+
+
+	key = parseURI(w.location).queryKey.gskey || parseURI(script.src).queryKey.gskey;
+
+	if (key) {
+		socket = io.connect();
+
+		socket.on('ready', function () {
+			d.addEventListener('click', sendEvent, false);
+
+
+			/*d.addEventListener('submit', function (e) {
+				e.preventDefault();
+				console.info(e);
+			}, false);*/
+
+
+			/*d.addEventListener('click', function (e) {
+				var node = e.target,
+					anchor;
+
+				while (node) {
+					if (node.nodeName.toUpperCase() === 'A') {
+						anchor = node;
+						break;
+					} else {
+						node = node.parentNode;
+					}
+				}
+
+				if (anchor && anchor.href) {
+					socket.emit('href', anchor.href);
+
+					console.info(getXPath(anchor));
+
+				}
+
+					e.preventDefault();
+
+			}, false);*/
+
+			window.onpopstate = function(event) {
+				socket.emit('href', d.location);
+			};
+
+
+		});
+
+		socket.emit('join', key);
+
+		isLeader = parseURI(w.location).queryKey.gsleader;
+
+		if (isLeader) {
+			//socket.emit('reload');
+		}
+
+		socket.on('href', function (href) {
+			w.location = href;
+		});
+
+		socket.on('reload', function () {
+			w.location.reload();
+		});
+
+		socket.on('event', receiveEvent);
+	}
 
 }(window, document));
 
@@ -95,10 +280,13 @@
 /*
 
 Get config
-    - id
-    - weinre
+	+ id
+	- weinre
 
-    capture user events: click, keydown, keypress, keyup, popstate
+	capture user events: click, popstate
 
+	fake back/forward buttons
+
+	domready
 
  */
--- server.js
+++ server.js
@@ -1,18 +1,50 @@
 var io = require('socket.io').listen(8001);
 
+io.static.add('/client.js', {
+	file: './client.js'
+});
+io.static.add('/test/integration/index.html', {
+	file: './test/integration/index.html',
+	mime: {
+		type: 'text/html',
+		encoding: 'utf8',
+		gzip: true
+	}
+});
+io.static.add('/test/integration/page2.html', {
+	file: './test/integration/page2.html',
+	mime: {
+		type: 'text/html',
+		encoding: 'utf8',
+		gzip: true
+	}
+});
+
+
 io.sockets.on('connection', function (socket) {
-  /*socket.emit('news', { hello: 'world' });*/
 
-  socket.on('join', function (id) {
-    socket.set('room', id, function () {
-        socket.emit('ready');
-    });
-    socket.join(id);
-  });
+	socket.on('join', function (key) {
+		socket.set('room', key, function () {
+				socket.emit('ready');
+		});
+		socket.join(key);
+	});
 
-  socket.on('href', function (href) {
-    socket.get('room', function (err, id) {
-      socket.broadcast.to(id).emit('href', href);
-    });
-  });
+	socket.on('href', function (href) {
+		socket.get('room', function (err, key) {
+			socket.broadcast.to(key).emit('href', href);
+		});
+	});
+
+	socket.on('event', function (e) {
+		socket.get('room', function (err, key) {
+			socket.broadcast.to(key).emit('event', e);
+		});
+	});
+
+	socket.on('reload', function () {
+		socket.get('room', function (err, key) {
+			socket.broadcast.to(key).emit('reload');
+		});
+	});
 });
